eb26837c15e09846f619373f72fb21fd7762ba3b
Dean.Chung <wenway13@gmail.com>
2020-01-28 17:20:22 +0800

    Adjust project os support from 11.0

---
+++ GithubSearchDemo/SimpleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleViewController.swift
+//  GithubSearchDemo
+//
+//  Created by Dean Chung on 2020/1/28.
+//  Copyright © 2020 Dean Chung. All rights reserved.
+//
+
+import UIKit
+
+class SimpleViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
